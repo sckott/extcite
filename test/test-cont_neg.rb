18816d955f4233f394a18460c8898bf6d20f37ff
Scott Chamberlain <myrmecocystus@gmail.com>
2019-08-07 14:58:02 -0700

    travis fixing

--- test/test-cont_neg.rb
+++ test/test-cont_neg.rb
@@ -5,6 +5,8 @@ class TestContNeg < Test::Unit::TestCase
   def test_cont_neg
     res = Extcite.cont_neg(ids: "10.1016/j.dendro.2014.01.004")
 
+    puts res.class
+
     # assert_equal(Array, res.class)
     assert_equal(String, res.class)
     assert_false(res.match(/Altman_2014/).nil?)

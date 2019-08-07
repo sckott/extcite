d10da0122e2bc5aa107d7b27bc2272a4f6154f7e
Scott Chamberlain <myrmecocystus@gmail.com>
2019-08-07 15:14:26 -0700

    travis fixing

--- test/test-cont_neg.rb
+++ test/test-cont_neg.rb
@@ -5,8 +5,11 @@ class TestContNeg < Test::Unit::TestCase
   def test_cont_neg
     res = Extcite.cont_neg(ids: "10.1016/j.dendro.2014.01.004")
 
-    # assert_equal(Array, res.class)
-    # assert_equal(String, res.class)
+    if res.class == Array
+        res = res[0]
+    end
+
+    assert_equal(String, res.class)
     assert_false(res.match(/Altman_2014/).nil?)
     assert_false(res.match(/Dendrochronologia/).nil?)
     assert_false(res.match(/TRADER/).nil?)

2686e9efe21ecc551c0f03ee8a76763cb4416bdb
Scott Chamberlain <myrmecocystus@gmail.com>
2019-11-21 09:57:58 -0800

    cleanup output file in extract call in test suite

--- test/test-extract.rb
+++ test/test-extract.rb
@@ -8,6 +8,8 @@ class TestExtract < Test::Unit::TestCase
 
   def test_extract_to_file
     res = Extcite.extract(path: @path)
+    # cleanup
+    File.unlink("out.bib")
 
     assert_equal(Array, res.class)
     assert_equal(1, res.length)
@@ -25,5 +27,4 @@ class TestExtract < Test::Unit::TestCase
     assert_true(res[0].match(/Ecology/).nil?)
     assert_true(res[0].match(/Some thoughts on best publishing practices/).nil?)
   end
-
 end

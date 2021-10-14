f8feafcbcecc802c510d18d9ac8655c38b94559e
Scott Chamberlain <sckott@fastmail.com>
2021-10-14 08:32:03 -0700

    fix test

--- test/test-cont_neg.rb
+++ test/test-cont_neg.rb
@@ -3,16 +3,16 @@ require_relative 'test_helper'
 class TestContNeg < Test::Unit::TestCase
 
   def test_cont_neg
-    res = Extcite.cont_neg(ids: "10.1016/j.dendro.2014.01.004")
+    res = Extcite.cont_neg(ids: "10.1002/ece3.8055")
 
     if res.class == Array
         res = res[0]
     end
 
     assert_equal(String, res.class)
-    assert_false(res.match(/Altman_2014/).nil?)
-    assert_false(res.match(/Dendrochronologia/).nil?)
-    assert_false(res.match(/TRADER/).nil?)
+    assert_false(res.match(/Wiley/).nil?)
+    assert_false(res.match(/Guzman/).nil?)
+    assert_false(res.match(/robustness/).nil?)
   end
 
 end
